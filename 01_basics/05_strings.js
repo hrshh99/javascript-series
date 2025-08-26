92fe79da72edfa89ef1126efb8bd2a54950f7d5a
Patel Harsh <163585965+hrshh99@users.noreply.github.com>
2025-08-26 08:59:52 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Harsh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Harsh-hp-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('h'));
+
+const  newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "    harsh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://harsh.com/harsh%20patel"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('harsh'))
+
+console.log(gameName.split('-'));

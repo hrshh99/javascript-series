54af0ab2325bf46136f5f57d13ce3988bd1a7b76
Patel Harsh <163585965+hrshh99@users.noreply.github.com>
2025-08-27 06:04:51 +0000

    taking parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,35 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Harsh"))
-console.log(loginUserMessage("Harsh"))
+// console.log(loginUserMessage("Harsh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Harsh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
